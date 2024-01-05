6517d6c48188677f13711977520766eded0ee4f7
Miguel Girón <mgironp@Miguels-MacBook-Pro.local>
2024-01-05 23:38:34 +0100

    Second commit

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/mgp87/go_learning/variables"
+import (
+	"fmt"
+
+	"github.com/mgp87/go_learning/variables"
+)
 
 func main() {
 	variables.ShowIntegers()
+	variables.RestOfVariables()
+	result, value := variables.ConvertToText(1776)
+	fmt.Println("Result: ", result)
+	fmt.Println("Value: ", value)
 }
---
+++ variables/rest.go
@@ -0,0 +1,30 @@
+package variables
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+var Name string
+var State bool
+var Salary float32
+var Date time.Time
+
+func RestOfVariables() {
+	Name = "Miguel"
+	State = true
+	Salary = 1500.69
+	Date = time.Now()
+
+	fmt.Println("Name: ", Name)
+	fmt.Println("Salary: ", Salary)
+	fmt.Println("State: ", State)
+	fmt.Println("Date: ", Date)
+}
+
+func ConvertToText(num int) (bool, string) {
+	var text string
+	text = strconv.Itoa(num)
+	return true, text
+}
